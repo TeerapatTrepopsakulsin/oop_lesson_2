56800f54b34d31be5dc66b43f41a6c9a5d87f2f8
TeerapatTrepopsakulsin <teerapat.tre@ku.th>
2023-11-14 21:34:37 +0700

    Correct gen_comb_list function

--- combination_gen.py
+++ combination_gen.py
@@ -15,17 +15,16 @@ def gen_comb_list(list_set):
             gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]) returns [[1, 4, 6], [2, 4, 6], [3, 4, 6], [1, 5, 6], [2, 5, 6], [3, 5, 6], [1, 4, 7], [2, 4, 7], [3, 4, 7], [1, 5, 7], [2, 5, 7], [3, 5, 7], [1, 4, 8], [2, 4, 8], [3, 4, 8], [1, 5, 8], [2, 5, 8], [3, 5, 8]]
     '''
 
-    def gen_comb_list_recursive(list_set):
-        if len(list_set) == 1:
-            start_list = []
-            for item in list_set[0]:
-                start_list.append([item])
-            return start_list
-        comb_list_temp = gen_comb_list_recursive(list_set[0:-1])
+    if len(list_set) == 1:
         start_list = []
-        for list_item in comb_list_temp:
-            for val in list_set[-1]:
-                temp_item = copy.deepcopy(list_item)
-                temp_item.append(val)
-                start_list.append(temp_item)
+        for item in list_set[0]:
+            start_list.append([item])
         return start_list
+    comb_list_temp = gen_comb_list(list_set[0:-1])
+    start_list = []
+    for list_item in comb_list_temp:
+        for val in list_set[-1]:
+            temp_item = copy.deepcopy(list_item)
+            temp_item.append(val)
+            start_list.append(temp_item)
+    return start_list
